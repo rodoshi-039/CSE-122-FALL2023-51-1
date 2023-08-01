2532fd6a58cc5bbb1513a80accc1d4b827d5d15e
Jafrin Nahar <140745661+rodoshi-039@users.noreply.github.com>
2023-08-01 10:22:06 +0600

    Update 2.Domino piling.cpp

--- Lab-4/2.Domino piling.cpp
+++ Lab-4/2.Domino piling.cpp
@@ -3,12 +3,23 @@ using namespace std;
 
 int main(){
 
-    int a,b;
-    cin>>a>>b;
+    int n;
+    cin>>n;
 
-    cout<<(a*b)/2;
+    int cnt=0;
+
+    for(int i=0;i<n;i++){
+        string x;
+        cin>>x;
+        if(x[1]=='+'){
+            cnt++;
+        }else{
+            cnt--;
+        }
+    }
+
+    cout<<cnt<<endl;
 
     return 0;
-
 }
 
