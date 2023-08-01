d96841ede45363c821346985668bdb504d4dede6
Jafrin Nahar <140745661+rodoshi-039@users.noreply.github.com>
2023-08-01 10:10:01 +0600

    Update 1.Next Round.cpp

--- Lab-4/1.Next Round.cpp
+++ Lab-4/1.Next Round.cpp
@@ -1,28 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int n,k,i,j,next=0;
-    cin>>n;
-    cin>>k;
-    int arr[n];
+    int a,b;
+    cin>>a>>b;
 
-    for(i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    int cnt=0;
 
-    for(j=0;j<n;j++){
-        if(arr[j]>=arr[k-1] && arr[j]>0){
-            next++;
+    for(int i=0;i<a;i++){
+        int x;
+        cin>>x;
+        if(x>b){
+            cnt++;
         }
     }
 
-
-    cout<<next;
-
+    cout<<cnt<<endl;
 
     return 0;
 }
-
- 
