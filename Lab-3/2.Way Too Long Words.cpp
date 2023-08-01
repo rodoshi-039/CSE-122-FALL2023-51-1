20f906ed3e78efa10d452b0b1924dcc6e72b749f
Jafrin Nahar <140745661+rodoshi-039@users.noreply.github.com>
2023-08-01 17:16:05 +0600

    Update 2.Way Too Long Words.cpp

--- Lab-3/2.Way Too Long Words.cpp
+++ Lab-3/2.Way Too Long Words.cpp
@@ -1,20 +1,15 @@
-#include<stdio.h>
-
+#include<iostream>
+using namespace std;
 int main()
 {
-    int i,n;
-    char word[100];
-
-    scanf("%d",&n);
-
-    for(i=1;i<=n;i++){
-        scanf("%s",&word);
-        if(strlen(word)>10){
-            printf("%c%d%c\n",word[0],strlen(word)-2,word[strlen(word)-1]);
-        }else{
-            printf("%s\n",word);
-        }
-    }
-
-    return 0;
+string y;
+cin>>y;
+if(y.size()<10)
+{
+cout<<y;
+}
+else
+{
+cout<<y[0]<<y.size()-2<<y[y.size()-1];
+}
 }
