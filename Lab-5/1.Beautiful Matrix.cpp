de260938e6afabd54022e3879bc0ed9c349d5212
Jafrin Nahar <140745661+rodoshi-039@users.noreply.github.com>
2023-08-08 16:05:44 +0600

    Update 1.Beautiful Matrix.cpp

--- Lab-5/1.Beautiful Matrix.cpp
+++ Lab-5/1.Beautiful Matrix.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int x = 0;
+    int x ;
+    int res;
     for (int i = 1; i <= 5; ++i)
     {
         for (int j = 1; j <= 5; ++j)
@@ -12,9 +13,9 @@ int main()
             cin >> x;
             if (x == 1)
             {
-                cout << abs(i - 3) + abs(j - 3) << endl;
-            }
+                 res= abs(i - 3) + abs(j - 3);            }
         }
     }
+    cout<<res;
     return 0;
 }
