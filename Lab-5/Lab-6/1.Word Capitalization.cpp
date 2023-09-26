ece71360f9fe172d87f5cd49b40fe1076219e961
Jafrin Nahar <140745661+rodoshi-039@users.noreply.github.com>
2023-09-26 10:46:32 +0600

    Create 1.Word Capitalization.cpp

---
+++ Lab-5/Lab-6/1.Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cin>>name;
+
+    char first=toupper(name[0]);
+    name[0]=first;
+    cout<<name<<endl;
+
+return 0;
+}
